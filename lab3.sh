6dd8f1c2a9d098646cfc68fc282d967fd9b94746
richard cusick <richard.cusick@mygeorgian.ca>
2024-08-01 21:33:23 -0400

    Changed error back and changed permissions on host machine to accept script

--- lab3.sh
+++ lab3.sh
@@ -25,11 +25,11 @@ done
 
 # Copy and execute the configure-host.sh script on server1
 scp configure-host.sh remoteadmin@server1-mgmt:/root
-ssh remoteadmin@server1-mgmt -- sudo /root/configure-host.sh $VERBOSE -name loghost -ip 192.168.16.3 -hostentry webhost 192.168.16.4
+ssh remoteadmin@server1-mgmt -- /root/configure-host.sh $VERBOSE -name loghost -ip 192.168.16.3 -hostentry webhost 192.168.16.4
 
 # Copy and execute the configure-host.sh script on server2
 scp configure-host.sh remoteadmin@server2-mgmt:/root
-ssh remoteadmin@server2-mgmt -- sudo /root/configure-host.sh $VERBOSE -name webhost -ip 192.168.16.4 -hostentry loghost 192.168.16.3
+ssh remoteadmin@server2-mgmt -- /root/configure-host.sh $VERBOSE -name webhost -ip 192.168.16.4 -hostentry loghost 192.168.16.3
 
 # Update the local /etc/hosts file
 ./configure-host.sh $VERBOSE -hostentry loghost 192.168.16.3
