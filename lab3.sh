382f57b81faf9c7fe25c9e09ea7ac8ddb35c918d
richard cusick <richard.cusick@mygeorgian.ca>
2024-08-01 20:54:38 -0400

    Created parses for command-line arguments for hostname, IP address, and host entry

--- lab3.sh
+++ lab3.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 # This script runs the configure-host.sh script from the current directory to modify two servers and update the local /etc/host file
 
+# Function to display help
+display_help() {
+	echo "Usage: $0 [--verbose]"
+    	exit 1
+}
+
+# Variables
+VERBOSE=""
+
+# Parsing command-line arguments
+while [[ "$#" -gt 0 ]]; do
+    	case $1 in
+        	--verbose)
+			VERBOSE="--verbose"
+            		shift
+            		;;
+        	*)
+            		display_help
+            		;;
+    	esac
+done
+
 # Copy and execute the configure-host.sh script on server1
 scp configure-host.sh remoteadmin@server1-mgmt:/root
 ssh remoteadmin@server1-mgmt -- /root/configure-host.sh $VERBOSE -name loghost -ip 192.168.16.3 -hostentry webhost 192.168.16.4
